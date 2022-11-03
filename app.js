a6fd877c92c02b08f1a66e042f75f1a3ead8eeb2
linahansten <skolkontolina@gmail.com>
2022-11-03 12:51:52 +0200

    added auth

--- app.js
+++ app.js
@@ -1,38 +1,77 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-app.js";
 import { getDatabase, ref, onChildAdded, set } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-database.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-auth.js";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCiL7D1h3ZaoXdyoMXbgQ7b8RaUc6jdQ9g",
-    authDomain: "pog22-8b683.firebaseapp.com",
-    projectId: "pog22-8b683",
-    storageBucket: "pog22-8b683.appspot.com",
-    messagingSenderId: "793659510891",
-    appId: "1:793659510891:web:2fd69d58c30ea247b7e12e",
-    databaseURL: "https://chat-prog22-default-rtdb.europe-west1.firebasedatabase.app"
-};
+    apiKey: "AIzaSyCCiY8ZCWjTIG47-zVZ_JTWGA0aUXIzDc0",
+    authDomain: "chat-prog22.firebaseapp.com",
+    databaseURL: "https://chat-prog22-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "chat-prog22",
+    storageBucket: "chat-prog22.appspot.com",
+    messagingSenderId: "958896767796",
+    appId: "1:958896767796:web:ad658e048f1a87f9591b61"
+}
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-// initializes Realtime Database and get a reference service
-const db = getDatabase(app);
 
-// create reference, where in the database we want to take info from
-const chatRef = ref(db, '/chat');
+// Auth
+//==========================================================
 
+//Set variable with Bootstrap modal
+const loginModal = new bootstrap.Modal('#login-modal')
+loginModal.show()
 
-// listens for database changes
-onChildAdded(chatRef, function (data) {
+// Listen for click on login button
+document.querySelector("#login-button").addEventListener("click", function () {
+    const email = document.querySelector("#email").value
+    const password = document.querySelector("#password").value
+    const auth = getAuth();
 
-    // create element and append to list element
-    const message = document.createElement("li")
-    message.innerText = new Date(data.key).toLocaleString("fi-FI") + ": " + data.val(); // copy message from input
+    // Sign in with Firebase
+    signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            // Signed in 
+            const user = userCredential.user;
+            console.log(user)
 
-    list.appendChild(message)
+            // Hide modal
+            loginModal.hide()
+
+            // Call function to init database
+            initDatabase()
+        })
+        .catch((error) => {
+            console.log(error)
+        });
+
 })
 
+//DataBase
+//======================================================
+
+// initializes Realtime Database and get a reference service
+function initDatabase() {
+    const db = getDatabase(app);
+
+    // create reference, where in the database we want to take info from
+    const chatRef = ref(db, '/chat');
+
+    // listens for database changes
+    onChildAdded(chatRef, function (data) {
+        // create element and append to list element
+        const list = document.querySelector("ul")
+        const message = document.createElement("li")
+
+        message.innerText = new Date(data.key).toLocaleString("fi-FI") + ": " + data.val(); // copy message from input
+
+        list.appendChild(message)
+    })
+}
+
+// New message 
 const input = document.querySelector("input");
-const list = document.querySelector("ul")
 
 input.addEventListener("keypress", function (event) {
     if (event.key == "Enter") {
@@ -40,10 +79,8 @@ input.addEventListener("keypress", function (event) {
         // create 'unique' id for message   
         const messageId = new Date().toUTCString()  // 27/10 9.52 message
 
-
         // send to database
         set(ref(db, "chat/" + messageId), input.value)
-
 
         // clear input
         input.value = "";
